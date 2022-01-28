5d68a0a756b3b8e6b871dbaf7994d3e6ad123f54
Benito <benitocavazosr@yahoo.com>
2022-01-28 01:21:46 -0600

    initalizing ip api routes

--- Develop/routes/htmlRoutes/index.js
+++ Develop/routes/htmlRoutes/index.js
@@ -3,8 +3,8 @@ const router = require('express').Router();
 
 
 
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/index.html'));
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/notes.html'));
   });
 
 
--- server.js
+++ server.js
@@ -7,12 +7,12 @@ const app = express();
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
-// app.use(express.json());
-// app.use(express.static('public'));
-// app.use('/api', apiRoutes);
-// app.use('/', htmlRoutes);
+app.use(express.json());
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
-const { notes } = require('./Develop/db/db');
+const { notes } = require('./Develop/db/db.json');
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
